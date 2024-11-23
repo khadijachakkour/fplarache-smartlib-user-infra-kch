e19fc788fce464151cc62d95dae061ecf6aa1754
khadija <Laptop@DESKTOP-0A8JGEO.masterdcc.larache>
2024-11-23 21:51:37 +0100

    Fixing Heredoc errors by using jsonencode

--- main.tf
+++ main.tf
@@ -1,33 +1,31 @@
-#Configuration des providers
+# Configuration des providers
 provider "aws" {  
   region = var.aws_region  
 }  
 
-#Création du repository ECR
+# Création du repository ECR
 resource "aws_ecr_repository" "app_repo" {  
   name                  = var.ecr_repository_name  
-  image_tag_mutability  = "MUABLE"  
+  image_tag_mutability  = "MUTABLE"  
 }
 
-#Création d’un cluster ECS
+# Création d'un cluster ECS
 resource "aws_ecs_cluster" "app_cluster" {
-    name = var.ecs_cluster_name
+  name = var.ecs_cluster_name
 }
 
-#Création de la task definition ECS
+# Création de la task definition ECS
 resource "aws_ecs_task_definition" "app_task" {  
   family                = var.ecs_task_family  
-  container_definitions  = <<DEFINITION  
-[  
-  {  
-    "name": "app",  
-    "image": "${aws_ecr_repository.app_repo.repository_url}:latest",  
-    "memory": 512,  
-    "cpu": 256,  
-    "essential": true  
-  }  
-]  
-DEFINITION  
+  container_definitions = jsonencode([  
+    {  
+      "name": "app",  
+      "image": "${aws_ecr_repository.app_repo.repository_url}:latest",  
+      "memory": 512,  
+      "cpu": 256,  
+      "essential": true  
+    }  
+  ])  
 
   requires_compatibilities = ["FARGATE"]  
   network_mode            = "awsvpc"  
@@ -36,7 +34,7 @@ DEFINITION
   execution_role_arn     = aws_iam_role.ecs_task_execution.arn  
 }
 
-#Création du service ECS
+# Création du service ECS
 resource "aws_ecs_service" "app_service" {  
   name            = var.ecs_service_name  
   cluster         = aws_ecs_cluster.app_cluster.id  
@@ -51,7 +49,7 @@ resource "aws_ecs_service" "app_service" {
   desired_count = 1  
 }
 
-#Rôle IAM pour ECS
+# Rôle IAM pour ECS
 resource "aws_iam_role" "ecs_task_execution" {  
   name = "ecsTaskExecutionRole"  
 
